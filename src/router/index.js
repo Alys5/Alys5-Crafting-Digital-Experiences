ca3ba5e261c5dec559accb1d0c84241e85f07cf9
Alys5 <alyssapeacock55@gmail.com>
2024-11-11 11:50:59 +0100

    manutenzione on

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 import MaintenancePage from '../view/MaintenancePage.vue'
 import HomePage from '../view/HomePage.vue'
 
+const isMaintenanceMode = true // Set to false to disable maintenance mode
+
 const routes = [
   { path: '/', name: 'Home', component: HomePage },
   { path: '/maintenance', name: 'Maintenance', component: MaintenancePage },
@@ -10,6 +12,15 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
+})
+
+// Redirect to maintenance page if maintenance mode is enabled and the current route is the home page
+router.beforeEach((to, from, next) => {
+  if (isMaintenanceMode && to.name === 'Home') {
+    next({ name: 'Maintenance' })
+  } else {
+    next()
+  }
 })
 
 export default router
